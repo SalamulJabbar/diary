9aaf0fdc5c8ca416bc87c98288e8f572d62a02d6
jabbarrr7 <salamuljabbar1005@gmail.com>
2023-10-14 15:11:58 +0700

    tambah file baru

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def save_diary():
     content_receive = request.form["content_give"]
 
     today = datetime.now()
-    mytime = today.strftime('%y-%m-%d %H-%M-%S')
+    mytime = today.strftime('%H-%M-%S '' %y-%m-%d ')
     
     file = request.files["file_give"]
     extension = file.filename.split('.')[-1]
@@ -38,6 +38,7 @@ def save_diary():
         'profile': profilename,
         'title':title_receive,
         'content':content_receive,
+        'mytime':mytime,
     }
     db.diary.insert_one(doc)
 
